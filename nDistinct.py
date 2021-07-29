0a06a703d3fdb13a1f3f6c777baddaef37e79050
lukechn99 <47294899+lukechn99@users.noreply.github.com>
2021-07-28 21:29:03 -0500

    adding nDistinct

--- nDistinct.py
+++ nDistinct.py
@@ -1,1 +1,25 @@
-# given a string, find the number of substrings that have a maximum of n distinct characters
+# given a string, find the number of substrings that have n distinct characters
+
+# use sliding window
+
+def distinct(string, ptr1, ptr2):
+    pass
+
+
+def nDistinct(string, n):
+    ptr1 = 0
+    ptr2 = n
+
+    number_distinct = 0
+
+    while ptr1 < len(string) - n:
+        while ptr2 < len(string):
+            if distinct(string, ptr1, ptr2):
+                number_distinct += 1
+            ptr2 += 1
+        ptr1 += 1
+        ptr2 = ptr1 + n
+    return number_distinct
+
+
+# we could also maintain a table of the characters and each time we slide we add or remove accordingly
--- splitPrimes.py
+++ splitPrimes.py
@@ -1,5 +1,14 @@
 # given a string of digits, count the number of ways the string can be split into prime numbers using the entire string
 
+# fun split(string):
+#     if the whole string is a prime:
+#         return 1
+#     ways = 0
+#     for each length of the string:
+#         if that length is prime:
+#             ways += split(rest of the string)
+#     return
+
 
 def isPrime(number):
     num = number
@@ -16,6 +25,17 @@ def countPrimeStrings(number, i):
         return 1
     cnt = 0
     for j in range(1, i + 1):
-        if (i - j >= 0 and number[i - j] != '0' and isPrime(int(number[i - j: i]))):
+        if (number[i - j] != '0' and isPrime(int(number[i - j: i]))):
             cnt += countPrimeStrings(number, i - j)
     return cnt
+
+
+if __name__ == "__main__":
+
+    s1 = "3175"
+    l1 = len(s1)
+    print(countPrimeStrings(s1, l1))
+
+    s2 = "11373"
+    l2 = len(s2)
+    print(countPrimeStrings(s2, l2))
